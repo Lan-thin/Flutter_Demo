405886b8f73154a46992a95de2caf467095819e0
First <815222890@qq.com>
2018-12-18 18:19:34 +0800

    暂存

--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // final wordPair = new WordPair.random();
-    return new MaterialApp(
-        title: 'Startup Name Generator',
-        theme: new ThemeData(
-          primaryColor: Colors.white,
-        ),
-        home: new RandomWords(),
-   );
-  }
+void main() {
+  runApp(
+    new MaterialApp(
+        title: 'app',
+        home: new MyScaffold(),
+    )
+  );
 }
-// 添加一个有状态的部件
-class RandomWords extends StatefulWidget{
-  @override
-  createState() =>
-      // TODO: implement createState
-      new RandomWordsState();
-}
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  final _saved = new Set<WordPair>();
-  @override
-  Widget build (BuildContext context) {
-    // final wordPair = new WordPair.random();
-    // return new Text(wordPair.asPascalCase);
-    return new Scaffold (
-      appBar: new AppBar(
-        title: new Text('startup Name Generator'),
-        actions: <Widget>[
-          new IconButton (icon: new Icon(Icons.list), onPressed: _pushSaved),
-        ],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return new Divider();
-        final index = i ~/2;
-        if(index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      }
-    );
-  }
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null
-      ),
-      onTap: ()  {
-        setState(()  {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-        builder: (context){
-          final tiles = _saved.map( (pair) {
-            return new ListTile(
-              title: new Text(
-                pair.asPascalCase,
-                style: _biggerFont,
-              ),
-            );
-          },
-          );
-          final divided = ListTile.divideTiles(
-           context: context,
-           tiles: tiles,
-          ).toList();
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text('Saved Suggestions'),
+class MyAppBar extends StatelessWidget {
+    MyAppBar({this.title});
+    
+    final Widget title;
+    @override
+    Widget build (BuildContext context) {
+        return new Container(
+            height: 56.0,
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            decoration: new BoxDecoration(color: Colors.blue[500]),
+            child: new Row(
+                children:<Widget>[
+                    new IconButton(
+                        icon: new Icon(Icons.menu),
+                        tooltip: 'Navigation menu',
+                        onPressed: null
+                    ),
+                    new Expanded(
+                        child: title,
+                    ),
+                    new IconButton(
+                        icon: new Icon(Icons.search),
+                        tooltip: 'Search',
+                        onPressed: null
+                    ),
+                ],
             ),
-            body: new ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
+        );
+    }
+}
+class MyScaffold extends StatelessWidget{
+    @override
+    Widget build(BuildContext context) {
+        return new Material(
+            child: new Column(
+                children: <Widget>[
+                    new MyAppBar(
+                        title: new Text(
+                            'Example title',
+                            style: Theme.of(context).primaryTextTheme.title
+                        ),
+                    ),
+                    new Expanded(
+                        child: new Center(
+                            child: new Text('hello World')
+                        ),
+                    ),
+                ],
+            ),
+        );
+    }
+}
+class TutorialHome extends StatelessWidget{
+    @override
+    Widget build(BuildContext context) {
+        return new MyScaffold(
+            appBar: new AppBar(
+                leading: new IconButton(
+                    icon: new Icon(Icons.menu),
+                    tooltip
+                ),
+            ),
+        );
+    }
 }
